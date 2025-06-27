3f7b7212a700f49dca84f8bdab8c7aef55d37fc2
zLuuuck <lucas.toterol.rodrigues@gmail.com>
2025-06-27 18:37:34 -0300

    setting up new archives

--- BlogGurizões/frontend/src/App.tsx
+++ BlogGurizões/frontend/src/App.tsx
@@ -1,9 +1,9 @@
 function App() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-black">
-      <h1 className="text-4xl font-bold text-blue-600">Tailwind está funcionando! 🚀</h1>
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600">Tailwind está funcionando! 🎉</h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
